a5dc74a3fd0a0565438f393c7a44e705fcea16ff
Nirmala <nirmala8579@gmail.com>
2025-08-28 15:32:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // frontend URL
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data); // broadcast to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
